7c84f18fef08300907c9c819204d79215ef06ed0
spect-o-sagar <59292252+spect-o-sagar@users.noreply.github.com>
2021-09-27 09:17:41 +0530

    updating test-2.py

--- examples/test-2.py
+++ examples/test-2.py
@@ -7,8 +7,6 @@ import pandas as pd
 
 url = 'https://kannada.asianetnews.com/central-government-jobs/hal-is-recruiting-apprentice-posts-and-check-details-qzbcph'
 topic = "engineering"
-cores = 6
-max_bert_len = 512
 
 
 if __name__ == '__main__':
@@ -18,7 +16,7 @@ if __name__ == '__main__':
     df = pd.read_csv('updated_finace_table.csv')
     for top,link in zip(df['topic'],df['Link2']):
       
-        text, mean = getLinkData(url=link, topic=top, language='kn', cores=6)
+        text, mean = getLinkData(url=link, topic=top, language='kn')
         if mean>threshold:
         # threashold adjustment code
             # if i>5:
